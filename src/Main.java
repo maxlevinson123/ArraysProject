27c22798741911dbb4cee8e9d48111164d800569
MaxLevinson <maxl77@nycstudents.net>
2026-02-05 08:46:09 -0500

    a

--- src/Main.java
+++ src/Main.java
@@ -15,8 +15,7 @@ public class Main {
                 String line = s.nextLine();
                 fileData += line + "\n";
             }
-        }
-        catch (FileNotFoundException e) {
+        } catch (FileNotFoundException e) {
             System.out.println("File not found");
         }
 
@@ -24,24 +23,39 @@ public class Main {
 
         int fiveOfAKind = 0;
         int fourOfAKind = 0;
-        int fullHouse= 0;
+        int fullHouse = 0;
         int threeOfAKind = 0;
         int twoPair = 0;
         int onePair = 0;
         int highCard = 0;
 
         for (String line : hands) {
+            String[] cardTypes = {"","","","",""};
             int[] counts = new int[5];
-            int i = 0;
 
             String[] hand = line.split("\\|");
             String cards = hand[0];
+            int betAmount = Integer.parseInt(hand[1]);
             String[] cardsArray = cards.split(",");
+            boolean inList = false;
 
             for (String card : cardsArray) {
-
+                for (int i = 0; i < cardTypes.length; i++) {
+                    if (card.equals(cardTypes[i])) {
+                        counts[i]++;
+                        inList = true;
+                        break;
+                    }
+                }
+                if (!inList) {
+                    for (int i = 0; i < cardTypes.length; i++) {
+                        if (cardTypes[i].equals("")) {
+                            cardTypes[i] = card;
+                        }
+                    }
+                }
             }
-        }
 
+        }
     }
 }
